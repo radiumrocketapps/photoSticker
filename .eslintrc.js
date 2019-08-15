130b97e66bb746c21f22129935ce381c64dbdd0c
Esteban Frare <esteban.frare@radiumrocket.com>
2019-08-15 01:34:36 -0300

    PS: config eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,29 @@
 module.exports = {
-  root: true,
-  extends: '@react-native-community',
+  env: {
+    browser: true,
+    es6: true,
+  },
+  extends: [
+    'airbnb',
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+  ],
+  rules: {
+    "semi": ["error", "never"],
+    "react/jsx-filename-extension": [1, { "extensions": [".js", ".jsx"] }],
+    'import/no-extraneous-dependencies': 0,
+    "no-unused-vars": [2, { "vars": "all", "args": "after-used" }]
+  },
 };
